6130ba08d645e907132fcce4b4fae323d1e713cd
RomaChauhan1607 <roma.chauhan167@gmail.com>
2023-10-02 15:11:44 +0530

    updated template

--- template.py
+++ template.py
@@ -12,6 +12,7 @@ list_of_files=[
 f"src/{package_name}/__init__.py",
 f"src/{package_name}/components/__init__.py",
 f"src/{package_name}/utils/__init__.py",
+f"src/{package_name}/config/__init__.py",
 f"src/{package_name}/pipeline/__init__.py",
 f"src/{package_name}/entity/__init__.py",
 f"src/{package_name}/constants/__init__.py",
